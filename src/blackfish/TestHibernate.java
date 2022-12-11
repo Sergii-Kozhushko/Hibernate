7c768a087bed9c94f15c70d705fb99694f58a24c
Sergii Kozhushko <s0504104302@gmail.com>
2022-12-11 17:07:31 +0100

    My first commit Changes to be committed: 	new file:   src/Main.java 	new file:   src/blackfish/TestHibernate.java

---
+++ src/blackfish/TestHibernate.java
@@ -0,0 +1,12 @@
+/**
+ * TestHibernate.java
+ *
+ * @author Sergii Kozhushko, sergiikozhushko@gmail.com
+ * Date of creation: 11-Dec-2022 16:56
+ */
+
+package blackfish;
+
+public class TestHibernate {
+
+}
